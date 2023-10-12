bde5071fd677561996c4d2744a85d1f54def336a
Le Quang <arceiusgaming@gmail.com>
2023-10-12 15:11:18 +0700

    12/10/2023

--- LMS_ELibrary/Program.cs
+++ LMS_ELibrary/Program.cs
@@ -2,6 +2,7 @@ using LMS_ELibrary.Model;
 using LMS_ELibrary.Services;
 using LMS_ELibrary.ServiceInterface;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //conect db
-builder.Services.AddDbContext<LMS_ELibraryContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("strConnect")));
+builder.Services.AddDbContext<LMS_ELibraryContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("strConnect"));
+
+}
+
+); ;
 
 //register service
 builder.Services.AddScoped<IUserService, UserService>();
@@ -21,12 +28,13 @@ builder.Services.AddScoped<IMonhocService, MonhocService>();
 builder.Services.AddScoped<IChudeService, ChudeService>();
 builder.Services.AddScoped<ITailieuService, TailieuService>();
 builder.Services.AddScoped<ILopgiangService, LopgiangService>();
-builder.Services.AddScoped<IBaigiangService, BaigiangService>();
+//builder.Services.AddScoped<IBaigiangService, BaigiangService>();
 builder.Services.AddScoped<IDethiService, DethiService>();
 builder.Services.AddScoped<ICauhoiService, CauhoiService>();
 builder.Services.AddScoped<IThongbaoService, ThongbaoService>();
 builder.Services.AddScoped<IHotroService, HotroService>();
 builder.Services.AddScoped<IHoidapService, HoidapService>();
+builder.Services.AddScoped<ITobomonService, TobomonService>();
 
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
